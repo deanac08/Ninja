602ac48965261b0b1ddcb2b9982467ac6b0d3cd5
deanac08 <deanac08@yahoo.com>
2022-01-14 05:30:09 -0800

    final

--- ninja.js
+++ ninja.js
@@ -7,17 +7,20 @@ class Ninja {
     }
 
     sayName(){
-        console.log('My name is ${this.name}');
+        console.log(`My name is ${this.name}`);
+        return this;
     }
 
     showStats(){
-        console.log('Name: ${this.name}');
-        console.log('Strength: ${this.strength}');
-        console.log('Speed: ${this.speed}');
-        console.log('Health: ${this.health}');
+        console.log(`Name: ${this.name}`);
+        console.log(`Strength: ${this.strength}`);
+        console.log(`Speed: ${this.speed}`);
+        console.log(`Health: ${this.health}`);
+        return this;
     }
 
     drinkSake(){
         this.health += 10;
+        return this;
     }
 }
